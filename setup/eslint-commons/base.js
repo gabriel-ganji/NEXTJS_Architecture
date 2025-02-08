224a7f2fcf95805d6b8e4f213e89949b98033583
Gabriel <gabrielganji01011010@gmail.com>
2025-02-07 21:26:32 -0300

    adding eslint linter to the project

--- setup/eslint-commons/base.js
+++ setup/eslint-commons/base.js
@@ -1,7 +1,12 @@
 module.exports = {
-    "extends": [
-        "next",
-        "eslint:recommended",
-        "plugin:prettier/recommended"
-    ]
-}
+    extends: [
+      "next",
+      "eslint:recommended",
+      "plugin:prettier/recommended"
+    ],
+    rules: {
+      "no-console": "error",
+      "@next/next/no-html-link-for-pages": "off",
+    },
+  };
+  
